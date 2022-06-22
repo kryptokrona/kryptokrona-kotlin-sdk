efb143ea4ee6f67e7fa1fb9541a137949eaf2b32
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-06-22 17:37:19 +0200

    Updating config file

--- src/main/java/org/kryptokrona/sdk/config/Config.java
+++ src/main/java/org/kryptokrona/sdk/config/Config.java
@@ -17,67 +17,67 @@ public class Config {
     /**
      * The amount of decimal places.
      */
-    private float                    decimalPlaces;
+    private static final int decimalPlaces                          = 5;
 
     /**
      * The address prefix Kryptokrona uses - you can find this in CryptoNoteConfig.h.
      */
-    private long                     addressPrefix;
+    private static final long addressPrefix                         = 2239254;
 
     /**
      * Request timeout for daemon operations in milliseconds.
      */
-    private long                     requestTimeout;
+    private static final long requestTimeout                        = 10 * 1000;
 
     /**
      * The block time of Kryptokrona, in seconds.
      */
-    private long                     blockTargetTime;
+    private static final long blockTargetTime                       = 90;
 
     /**
      * How often to process blocks, in millseconds.
      */
-    private long                     syncThreadInterval;
+    private static final long syncThreadInterval                    = 10;
 
     /**
      * How often to update the daemon info.
      */
-    private long                     daemonUpdateInterval;
+    private static final long daemonUpdateInterval                  = 10 * 1000;
 
     /**
      * How often to check on locked transactions.
      */
-    private long                     lockedTransactionsCheckInterval;
+    private static final long lockedTransactionsCheckInterval       = 30 * 1000;
 
     /**
      * The amount of blocks to process per 'tick' of the mainloop. Note: too
      * high a value will cause the event loop to be blocked, and your interaction
      * to be laggy.
      */
-    private long                     blocksPerTick;
+    private static final long blocksPerTick                         = 1;
 
     /**
      * Kryptokrona 'ticker'
      */
-    private String                  ticker;
+    private static final String ticker                              = "XKR";
 
     /**
      * Most people haven't mined any blocks, so lets not waste time scanning
      * them.
      */
-    private boolean                  scanCoinbaseTransactions;
+    private static final boolean scanCoinbaseTransactions           = false;
 
     /**
      * The minimum fee allowed for transactions, in ATOMIC units.
      */
-    private long                     minimumFee;
+    private static final long minimumFee                            = 10;
 
     /**
      * Fee per byte is rounded up in chunks. This helps makes estimates
      * more accurate. It's suggested to make this a power of two, to relate
      * to the underlying storage cost / page sizes for storing a transaction.
      */
-    private int                      feePerByteChunkSize;
+    private static final int feePerByteChunkSize                    = 256;
 
     /** Fee to charge per byte of transaction. Will be applied in chunks, see
      * above. This value comes out to 1.953125. We use this value instead of
@@ -85,78 +85,12 @@ public class Config {
      * - 5 XKR vs 5.12 XKR. You can read this as the fee per chunk
      * is 500 atomic units. The fee per byte is 500 / chunk size.
      */
-    private double                   minimumFeePerByte;
+    private static final double minimumFeePerByte                   = 500.00 / feePerByteChunkSize;
 
     /**
      * Mapping of height to mixin maximum and mixin minimum.
      */
-    public MixinLimits               mixinLimits;
-
-    /**
-     * The length of a standard address for Kryptokrona.
-     */
-    private long                     standardAddressLength;
-
-    /**
-     * The length of an integrated address for Kryptokrona - It's the same as
-     * a normal address, but there is a paymentID included in there - since
-     * payment ID's are 64 chars, and base58 encoding is done by encoding
-     * chunks of 8 chars at once into blocks of 11 chars, we can calculate
-     * this automatically.
-     */
-    private long                     integratedAddressLength;
-
-    /**
-     * The amount of memory to use storing downloaded blocks - 50MB
-     */
-    private long                     blockStoreMemoryLimit;
-
-    /**
-     * The amount of blocks to take from the daemon per request. Cannot take
-     * more than 100.
-     */
-    private long                     blocksPerDaemonRequest;
-
-    /**
-     * The amount of seconds to permit not having fetched a block from the
-     * daemon before emitting 'deadnode'. Note that this just means contacting
-     * the daemon for data - if you are synced and it returns TopBlock - the
-     * event will not be emitted.
-     */
-    private long                     maxLastFetchedBlockInterval;
-
-    /**
-     * The amount of seconds to permit not having fetched a new network height
-     * from the daemon before emitting 'deadnode'.
-     */
-    private long                     maxLastUpdatedNetworkHeightInterval;
-
-    /**
-     * The amount of seconds to permit not having fetched a new local height
-     * from the daemon before emitting 'deadnode'.
-     */
-    private long                     maxLastUpdatedLocalHeightInterval;
-
-    /**
-     * Allows setting a custom user agent string
-     */
-    private String                   customUserAgentString;
-
-    public Config() {
-        this.decimalPlaces                          = 5;
-        this.addressPrefix                          = 2239254;
-        this.requestTimeout                         = 10 * 1000;
-        this.blockTargetTime                        = 90;
-        this.syncThreadInterval                     = 10;
-        this.daemonUpdateInterval                   = 10 * 1000;
-        this.lockedTransactionsCheckInterval        = 30 * 1000;
-        this.blocksPerTick                          = 1;
-        this.ticker                                 = "XKR";
-        this.scanCoinbaseTransactions               = false;
-        this.minimumFee                             = 10;
-        this.feePerByteChunkSize                    = 256;
-        this.minimumFeePerByte                      = 500.00 / this.feePerByteChunkSize;
-        this.mixinLimits                            = new MixinLimits(Arrays.asList(
+    public static final MixinLimits mixinLimits                     = new MixinLimits(Arrays.asList(
                 // Height: 440,000, minMixin: 0, maxMixin: 100, defaultMixin: 3
                 new MixinLimit(440000, 0, 100, 3),
 
@@ -165,14 +99,57 @@ public class Config {
 
                 // At height of 800000, static mixin of 3
                 new MixinLimit(800000, 3)
-        ), 3);
-        this.standardAddressLength                  = 99;
-        this.integratedAddressLength                = 99 + ((64 * 11) / 8);
-        this.blockStoreMemoryLimit                  = 1024 * 1024 * 50;
-        this.blocksPerDaemonRequest                 = 100;
-        this.maxLastFetchedBlockInterval            = 60 * 3;
-        this.maxLastUpdatedNetworkHeightInterval    = 60 * 3;
-        this.maxLastUpdatedLocalHeightInterval      = 60 * 3;
-        this.customUserAgentString                  = String.format("%s-sdk-%s", this.ticker.toLowerCase(), System.getProperty("sdk-version"));
-    }
+            ), 3);
+
+    /**
+     * The length of a standard address for Kryptokrona.
+     */
+    private static final long standardAddressLength                 = 99;
+
+    /**
+     * The length of an integrated address for Kryptokrona - It's the same as
+     * a normal address, but there is a paymentID included in there - since
+     * payment ID's are 64 chars, and base58 encoding is done by encoding
+     * chunks of 8 chars at once into blocks of 11 chars, we can calculate
+     * this automatically.
+     */
+    private static final long integratedAddressLength               = 99 + ((64 * 11) / 8);
+
+    /**
+     * The amount of memory to use storing downloaded blocks - 50MB
+     */
+    private static final long blockStoreMemoryLimit                 = 1024 * 1024 * 50;
+
+    /**
+     * The amount of blocks to take from the daemon per request. Cannot take
+     * more than 100.
+     */
+    private static final long blocksPerDaemonRequest                = 100;
+
+    /**
+     * The amount of seconds to permit not having fetched a block from the
+     * daemon before emitting 'deadnode'. Note that this just means contacting
+     * the daemon for data - if you are synced and it returns TopBlock - the
+     * event will not be emitted.
+     */
+    private static final long maxLastFetchedBlockInterval           = 60 * 3;
+
+    /**
+     * The amount of seconds to permit not having fetched a new network height
+     * from the daemon before emitting 'deadnode'.
+     */
+    private static final long maxLastUpdatedNetworkHeightInterval   = 60 * 3;
+
+    /**
+     * The amount of seconds to permit not having fetched a new local height
+     * from the daemon before emitting 'deadnode'.
+     */
+    private static final long maxLastUpdatedLocalHeightInterval     = 60 * 3;
+
+    /**
+     * Allows setting a custom user agent string
+     */
+    private static final String customUserAgentString               = String.format(
+            "%s-sdk-%s", ticker.toLowerCase(), System.getProperty("sdk-version"));
+
 }
