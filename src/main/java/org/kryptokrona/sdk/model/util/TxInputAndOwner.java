4dcb33a1ba7822a18808d72d1fd5d772033004d8
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-07-07 15:56:34 +0200

    Adding implementation of TxInputAndOwner and UnconfirmedInput

--- src/main/java/org/kryptokrona/sdk/model/util/TxInputAndOwner.java
+++ src/main/java/org/kryptokrona/sdk/model/util/TxInputAndOwner.java
@@ -1,4 +1,27 @@
 package org.kryptokrona.sdk.model.util;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.kryptokrona.sdk.model.transaction.TransactionInput;
+
+/**
+ * TxInputAndOwner.java
+ *
+ * @author Marcus Cvjeticanin (@mjovanc)
+ */
+@Getter
+@Setter
+@NoArgsConstructor
 public class TxInputAndOwner {
+
+    private TransactionInput    input;
+    private String              privateSpendKey;
+    private String              publicSpendKey;
+
+    public TxInputAndOwner(TransactionInput input, String privateSpendKey, String publicSpendKey) {
+        this.input = input;
+        this.privateSpendKey = privateSpendKey;
+        this.publicSpendKey = publicSpendKey;
+    }
 }
--- src/main/java/org/kryptokrona/sdk/model/util/UnconfirmedInput.java
+++ src/main/java/org/kryptokrona/sdk/model/util/UnconfirmedInput.java
@@ -1,4 +1,26 @@
 package org.kryptokrona.sdk.model.util;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * UnconfirmedInput.java
+ *
+ * @author Marcus Cvjeticanin (@mjovanc)
+ */
+@Getter
+@Setter
+@NoArgsConstructor
 public class UnconfirmedInput {
+
+    private double amount;
+    private String key;
+    private String parentTransactionHash;
+
+    public UnconfirmedInput(double amount, String key, String parentTransactionHash) {
+        this.amount = amount;
+        this.key = key;
+        this.parentTransactionHash = parentTransactionHash;
+    }
 }
