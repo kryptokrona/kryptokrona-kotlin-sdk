efa98bed0315cd692119de7f05c8441677839a8f
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-08-31 16:03:07 +0200

    Implementing ParentBlock.java

--- src/main/java/org/kryptokrona/sdk/block/ParentBlock.java
+++ src/main/java/org/kryptokrona/sdk/block/ParentBlock.java
@@ -1,4 +1,55 @@
 package org.kryptokrona.sdk.block;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.kryptokrona.sdk.transaction.Transaction;
+
+import java.util.List;
+
+/**
+ * ParentBlock.java
+ *
+ * @author Marcus Cvjeticanin (@mjovanc)
+ */
+@Getter
+@Setter
+@NoArgsConstructor
 public class ParentBlock {
+
+	/**
+	 * The major block version number
+	 */
+	private int majorVersion;
+
+	/**
+	 * The minor block version number
+	 */
+	private int minorVersion;
+
+	/**
+	 * The previous block hash
+	 */
+	private String previousBlockHash;
+
+	/**
+	 * The number of transactions in the block
+	 */
+	private long transactionCount;
+
+	/**
+	 * The base transaction branches
+	 */
+	private List<String> baseTransactionBranch;
+
+	/**
+	 * The miner reward transaction
+	 */
+	private Transaction minerTransaction;
+
+	/**
+	 * The blockchain branches
+	 */
+	private List<String> blockchainBranch;
+
 }
