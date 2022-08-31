5d57137d38970b976ab76145c7f717a5d237b7c7
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-08-31 18:28:57 +0200

    Adding KeyPair and Keys implementation

--- src/main/java/org/kryptokrona/sdk/crypto/KeyPair.java
+++ src/main/java/org/kryptokrona/sdk/crypto/KeyPair.java
@@ -1,4 +1,13 @@
 package org.kryptokrona.sdk.crypto;
 
+/**
+ * KeyPair.java
+ *
+ * @author Marcus Cvjeticanin (@mjovanc)
+ */
 public class KeyPair {
+
+	private String mPrivateKey;
+
+	private String mPublicKey;
 }
--- src/main/java/org/kryptokrona/sdk/crypto/Keys.java
+++ src/main/java/org/kryptokrona/sdk/crypto/Keys.java
@@ -1,4 +1,23 @@
 package org.kryptokrona.sdk.crypto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Keys.java
+ *
+ * @author Marcus Cvjeticanin (@mjovanc)
+ */
+@Getter
+@Setter
 public class Keys {
+
+	private KeyPair mSpendKeys;
+
+	private KeyPair mViewKeys;
+
+	public Keys(KeyPair mSpendKeys, KeyPair mViewKeys) {
+		this.mSpendKeys = mSpendKeys;
+		this.mViewKeys = mViewKeys;
+	}
 }
