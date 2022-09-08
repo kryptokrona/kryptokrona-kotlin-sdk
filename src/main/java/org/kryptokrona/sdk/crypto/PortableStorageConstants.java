64fd80b4bba595fce6ecd51e456e546646ea19f5
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-08 15:43:42 +0200

    Adding implementation in enum PortableStorageConstants

--- src/main/java/org/kryptokrona/sdk/crypto/PortableStorageConstants.java
+++ src/main/java/org/kryptokrona/sdk/crypto/PortableStorageConstants.java
@@ -1,4 +1,22 @@
 package org.kryptokrona.sdk.crypto;
 
+/**
+ * PortableStorageConstants.java
+ *
+ * @author Marcus Cvjeticanin (@mjovanc)
+ */
 public enum PortableStorageConstants {
+	SIGNATURE_A(0x01011101),
+	SIGNATURE_B(0x01020101),
+	VERSION(1);
+
+	private int code;
+
+	PortableStorageConstants(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
 }
