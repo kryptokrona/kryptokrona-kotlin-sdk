d7184adba4fbe13793daad45ae4f812e3ac78568
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-08 16:07:54 +0200

    Adding implementation to LedgerAPDU enum

--- src/main/java/org/kryptokrona/sdk/crypto/LedgerAPDU.java
+++ src/main/java/org/kryptokrona/sdk/crypto/LedgerAPDU.java
@@ -1,4 +1,23 @@
 package org.kryptokrona.sdk.crypto;
 
+/**
+ * LedgerAPDU.java
+ *
+ * @author Marcus Cvjeticanin (@mjovanc)
+ */
 public enum LedgerAPDU {
+	P2(0x00),
+	P1_NON_CONFIRM(0x00),
+	P1_CONFIRM(0x01),
+	INS(0xe0);
+
+	private int code;
+
+	LedgerAPDU(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
 }
