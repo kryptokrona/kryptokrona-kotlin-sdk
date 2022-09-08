b8c0992394b663903f1561f4a1464fb0ddd1cc02
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-08 16:17:53 +0200

    Adding implementation to LedgerCommand enum

--- src/main/java/org/kryptokrona/sdk/crypto/LedgerCommand.java
+++ src/main/java/org/kryptokrona/sdk/crypto/LedgerCommand.java
@@ -1,4 +1,52 @@
 package org.kryptokrona.sdk.crypto;
 
+/**
+ * LedgerCommand.java
+ *
+ * @author Marcus Cvjeticanin (@mjovanc)
+ */
 public enum LedgerCommand {
+	VERSION(0x01),
+	DEBUG(0x02),
+	IDENT(0x05),
+	PUBLIC_KEYS(0x10),
+	VIEW_SECRET_KEY(0x11),
+	SPEND_SECRET_KEY(0x12),
+	WALLET_KEYS(0x13),
+	CHECK_KEY(0x16),
+	CHECK_SCALAR(0x17),
+	PRIVATE_TO_PUBLIC(0x18),
+	RANDOM_KEY_PAIR(0x19),
+	ADDRESS(0x30),
+	GENERATE_KEY_IMAGE(0x40),
+	GENERATE_KEY_IMAGE_PRIMITIVE(0x41),
+	GENERATE_RING_SIGNATURES(0x50),
+	COMPLETE_RING_SIGNATURE(0x51),
+	CHECK_RING_SIGNATURES(0x52),
+	GENERATE_SIGNATURE(0x55),
+	CHECK_SIGNATURE(0x56),
+	GENERATE_KEY_DERIVATION(0x60),
+	DERIVE_PUBLIC_KEY(0x61),
+	DERIVE_SECRET_KEY(0x62),
+	TX_STATE(0x70),
+	TX_START(0x71),
+	TX_START_INPUT_LOAD(0x72),
+	TX_LOAD_INPUT(0x73),
+	TX_START_OUTPUT_LOAD(0x74),
+	TX_LOAD_OUTPUT(0x75),
+	TX_FINALIZE_TX_PREFIX(0x76),
+	TX_SIGN(0x77),
+	TX_DUMP(0x78),
+	TX_RESET(0x79),
+	RESET_KEYS(0xff);
+
+	private int code;
+
+	LedgerCommand(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
 }
