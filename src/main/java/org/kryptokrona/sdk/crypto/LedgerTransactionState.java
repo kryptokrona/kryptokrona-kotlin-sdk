b12624cf71c6f83b13dfcfe9ff0c0223a26fb156
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-08 16:06:32 +0200

    Adding implementation to LedgerTransactionState enum

--- src/main/java/org/kryptokrona/sdk/crypto/LedgerTransactionState.java
+++ src/main/java/org/kryptokrona/sdk/crypto/LedgerTransactionState.java
@@ -1,4 +1,27 @@
 package org.kryptokrona.sdk.crypto;
 
+/**
+ * LedgerTransactionState.java
+ *
+ * @author Marcus Cvjeticanin (@mjovanc)
+ */
 public enum LedgerTransactionState {
+	INACTIVE(0x00),
+	READY(0x01),
+	RECEIVING_INPUTS(0x02),
+	INPUTS_RECEIVED(0x03),
+	RECEIVING_OUTPUTS(0x04),
+	OUTPUTS_RECEIVED(0x05),
+	PREFIX_READY(0x06),
+	COMPLETE(0x07);
+
+	private int code;
+
+	LedgerTransactionState(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
 }
