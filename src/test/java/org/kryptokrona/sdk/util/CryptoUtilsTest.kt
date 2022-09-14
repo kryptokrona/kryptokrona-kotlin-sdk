0f1d5306496df36c2228bb43978474e12a53fe4f
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-14 18:57:27 +0200

    Adding new unit test for can not enter invalid mnemonic word

--- src/test/java/org/kryptokrona/sdk/util/CryptoUtilsTest.kt
+++ src/test/java/org/kryptokrona/sdk/util/CryptoUtilsTest.kt
@@ -1,11 +1,18 @@
 package org.kryptokrona.sdk.util;
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class CryptoUtilsTest {
 
     @Test
     fun `can enter valid mnemonic word`() {
-        CryptoUtils.isValidMnemonicWord("adapt")
+        assertTrue(CryptoUtils.isValidMnemonicWord("adapt"));
+    }
+
+    @Test
+    fun `can not enter invalid mnemonic word`() {
+        assertFalse(CryptoUtils.isValidMnemonicWord("mjovanc"));
     }
 }
