c94de80c1621b88ef41d7d41d34e36efcc0ec356
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-10-27 01:22:03 +0200

    Removing unused import

--- src/test/java/org/kryptokrona/sdk/validator/WalletValidatorTest.kt
+++ src/test/java/org/kryptokrona/sdk/validator/WalletValidatorTest.kt
@@ -1,6 +1,5 @@
 package org.kryptokrona.sdk.validator;
 
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.kryptokrona.sdk.exception.wallet.*
 import org.kryptokrona.sdk.model.util.FeeType
