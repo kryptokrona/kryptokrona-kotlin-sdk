9c17924c9f65459db1aadc9b3f1e30d289b0e4d7
mjovanc <mjovanc@icloud.com>
2023-03-07 23:00:41 +0100

    Adding slf4j dependencies

--- kryptokrona-http/build.gradle.kts
+++ kryptokrona-http/build.gradle.kts
@@ -1,5 +1,6 @@
 val ktor_version: String by project
 val coroutines_version: String by project
+val slf4j_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.10"
@@ -19,6 +20,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("org.slf4j:slf4j-api:$slf4j_version")
+    implementation("org.slf4j:slf4j-simple:$slf4j_version")
 
     testImplementation(kotlin("test"))
 }
--- kryptokrona-http/src/main/kotlin/org/kryptokrona/http/Main.kt
+++ kryptokrona-http/src/main/kotlin/org/kryptokrona/http/Main.kt
@@ -1,16 +1,12 @@
 package org.kryptokrona.http
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.kryptokrona.http.common.get
 
 
 fun main(args: Array<String>) {
-    GlobalScope.launch {
-        get()
-        delay(5000)
-    }
+
+    runBlocking { get() }
 
     print("Ending coroutine...")
 }
