fa4c623ec7078a260ff8386bfb0bf4c651df0cd3
mjovanc <mjovanc@icloud.com>
2023-03-07 18:59:56 +0100

    Adding kryptokrona-core lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,54 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("com.dorongold.task-tree") version "2.1.0"
-    kotlin("jvm") version "1.7.10"
-    application
+    kotlin("jvm") version "1.8.10"
 }
-
-group "org.kryptokrona.sdk"
-
-// getting project properties (gradle.properties)
-val sdkVersion: String by project
 
 repositories {
     mavenCentral()
 }
 
-sourceSets {
-    main {
-        resources {
-            srcDirs ("src/main/resources")
-        }
-    }
-}
-
 dependencies {
-    // implementation
-    implementation("io.github.classgraph:classgraph:4.8.149")
-    implementation("com.github.seancfoley:ipaddress:5.3.4")
-    implementation("com.fasterxml.jackson:jackson-bom:2.13.4")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.1")
-    implementation("org.slf4j:slf4j-api:2.0.0")
-    implementation("org.apache.httpcomponents.client5:httpclient5:5.1.3")
-	implementation("org.apache.httpcomponents.client5:httpclient5-fluent:5.1.3")
-    implementation("org.slf4j:slf4j-simple:2.0.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.18.0")
-
-    // test
-    testImplementation(kotlin("stdlib-jdk8"))
     testImplementation(kotlin("test"))
-    testCompileOnly("org.projectlombok:lombok:1.18.24")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
-
-    // various
-    compileOnly("org.projectlombok:lombok:1.18.24")
-    annotationProcessor("org.projectlombok:lombok:1.18.24")
 }
 
-tasks.withType<Test> {
+tasks.named<Test>("test") {
     useJUnitPlatform()
-    failFast = true
+    // failFast = true
 }
 
 tasks.withType<KotlinCompile> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,4 @@
 rootProject.name = "kryptokrona-sdk"
+
+// include libraries
+include("kryptokrona-core")
