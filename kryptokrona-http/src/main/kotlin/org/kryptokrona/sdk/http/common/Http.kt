e34e31dcfb2cc6f2d5a715aba6378fd3debf3039
mjovanc <mjovanc@icloud.com>
2023-04-13 21:39:14 +0200

    Adding HttpRequestRetry to Http client

--- kryptokrona-http/src/main/kotlin/org/kryptokrona/sdk/http/common/Http.kt
+++ kryptokrona-http/src/main/kotlin/org/kryptokrona/sdk/http/common/Http.kt
@@ -31,6 +31,7 @@
 package org.kryptokrona.sdk.http.common
 
 import io.ktor.client.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
@@ -41,6 +42,10 @@ import io.ktor.serialization.kotlinx.json.*
 private val client = HttpClient {
     install(ContentNegotiation) {
         json()
+    }
+    install(HttpRequestRetry) {
+        retryOnServerErrors(maxRetries = 5)
+        exponentialDelay()
     }
 }
 
