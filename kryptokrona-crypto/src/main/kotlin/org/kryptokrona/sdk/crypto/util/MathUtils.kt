c08b92e424c3b5eb48f9ccd31f3bc7a2bac83ebe
mjovanc <mjovanc@icloud.com>
2023-04-15 16:32:22 +0200

    Deleting util file

--- kryptokrona-crypto/src/main/kotlin/org/kryptokrona/sdk/crypto/util/MathUtils.kt
+++
@@ -1,7 +0,0 @@
-package org.kryptokrona.sdk.crypto.util
-
-import org.kryptokrona.sdk.crypto.CLibraryLoader
-
-class MathUtils : CLibraryLoader() {
-    external fun add(a: Int, b: Int): Int
-}
--- playground/src/main/kotlin/CryptoLib.kt
+++ playground/src/main/kotlin/CryptoLib.kt
@@ -1,9 +1,5 @@
 package org.kryptokrona.sdk.crypto
 
-import org.kryptokrona.sdk.crypto.util.MathUtils
-
 fun main() {
-    val math = MathUtils()
-    println(math.add(2, 3))
     println("Hello, world!")
 }
