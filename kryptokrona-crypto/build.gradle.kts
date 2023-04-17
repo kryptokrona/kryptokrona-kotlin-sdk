7411de6bdf2baaac52b338f22b12717c565e69be
mjovanc <mjovanc@icloud.com>
2023-04-18 01:08:46 +0200

    Adding kover report for kryptokrona-crypto lib

--- kryptokrona-crypto/build.gradle.kts
+++ kryptokrona-crypto/build.gradle.kts
@@ -37,6 +37,36 @@ kotlin {
     jvmToolchain(17)
 }
 
+koverReport {
+    filters {
+        excludes {
+            classes("org.kryptokrona.sdk.crypto.hugin.*")
+            classes("org.kryptokrona.sdk.crypto.model.*")
+        }
+    }
+
+    verify {
+        onCheck = true
+        rule {
+            isEnabled = true
+            entity = kotlinx.kover.gradle.plugin.dsl.GroupingEntityType.APPLICATION
+
+            filters {
+                excludes {
+                    classes("org.kryptokrona.sdk.http.model.*")
+                }
+            }
+
+            bound {
+                minValue = 60
+                maxValue = 90
+                metric = kotlinx.kover.gradle.plugin.dsl.MetricType.LINE
+                aggregation = kotlinx.kover.gradle.plugin.dsl.AggregationType.COVERED_PERCENTAGE
+            }
+        }
+    }
+}
+
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
