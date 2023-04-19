7347fe072464ed48016e98b3786e55d2d95b5e21
mjovanc <mjovanc@icloud.com>
2023-04-19 22:09:25 +0200

    Adding more unit tests to WalletClientTest

--- kryptokrona-http/src/test/kotlin/org/kryptokrona/sdk/http/client/WalletClientTest.kt
+++ kryptokrona-http/src/test/kotlin/org/kryptokrona/sdk/http/client/WalletClientTest.kt
@@ -40,39 +40,52 @@ class WalletClientTest {
 
     @Test
     @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
-    fun `can get wallet sync data with request body` () = runTest {
+    fun `can get wallet sync data with start height` () = runTest {
         // Arrange
-        val requestData1 = WalletSyncDataRequest(startHeight = 10000)
-        val requestData2 = WalletSyncDataRequest(startHeight = 100000, startTimestamp = 50, blockCount = 100)
-        val requestData3 = WalletSyncDataRequest(startHeight = 10000, startTimestamp = 100000, blockCount = 50)
+        val requestData = WalletSyncDataRequest(startHeight = 10000)
 
         // Act
-        val dataHTTP1 = clientHTTP.getWalletSyncData(requestData1)
-        val dataHTTP2 = clientHTTP.getWalletSyncData(requestData2)
-        val dataHTTP3 = clientHTTP.getWalletSyncData(requestData3)
-
-        val dataHTTPS1 = clientHTTPS.getWalletSyncData(requestData1)
-        val dataHTTPS2 = clientHTTPS.getWalletSyncData(requestData2)
-        val dataHTTPS3 = clientHTTPS.getWalletSyncData(requestData3)
+        val dataHTTP = clientHTTP.getWalletSyncData(requestData)
+        val dataHTTPS = clientHTTPS.getWalletSyncData(requestData)
 
         // Assert
-        if (dataHTTP1 != null) assertEquals(dataHTTP1.status, "OK")
-        if (dataHTTP2 != null) assertEquals(dataHTTP2.status, "OK")
-        if (dataHTTP3 != null) assertEquals(dataHTTP3.status, "OK")
-        if (dataHTTPS1 != null) assertEquals(dataHTTPS1.status, "OK")
-        if (dataHTTPS2 != null) assertEquals(dataHTTPS2.status, "OK")
-        if (dataHTTPS3 != null) assertEquals(dataHTTPS3.status, "OK")
+        if (dataHTTP != null) assertEquals(dataHTTP.status, "OK")
+        if (dataHTTPS != null) assertEquals(dataHTTPS.status, "OK")
 
-        dataHTTP1?.items?.get(0)?.let { assertEquals(it.blockHeight, 10000) }
-        dataHTTP2?.items?.get(0)?.let { assertEquals(it.blockHeight, 100000) }
-        dataHTTP3?.items?.get(0)?.let { assertEquals(it.blockHeight, 10000) }
-        dataHTTPS1?.items?.get(0)?.let { assertEquals(it.blockHeight, 10000) }
-        dataHTTPS2?.items?.get(0)?.let { assertEquals(it.blockHeight, 100000) }
-        dataHTTPS3?.items?.get(0)?.let { assertEquals(it.blockHeight, 10000) }
+        dataHTTP?.items?.get(0)?.let { assertEquals(it.blockHeight, 10000) }
+        dataHTTPS?.items?.get(0)?.let { assertEquals(it.blockHeight, 10000) }
 
-        assertEquals(100, dataHTTP2?.items?.size)
-        assertEquals(50, dataHTTP3?.items?.size)
-        assertEquals(100, dataHTTPS2?.items?.size)
-        assertEquals(50, dataHTTPS3?.items?.size)
+        assertEquals(100, dataHTTP?.items?.size)
+        assertEquals(100, dataHTTPS?.items?.size)
+    }
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can get wallet sync data with start height and start timestamp`() = runTest {
+        // Arrange
+        val requestData = WalletSyncDataRequest(startHeight = 100000, startTimestamp = 50)
+
+        // Act
+        val dataHTTP = clientHTTP.getWalletSyncData(requestData)
+        val dataHTTPS = clientHTTPS.getWalletSyncData(requestData)
+
+        // Assert
+        if (dataHTTP != null) assertEquals(dataHTTP.status, "OK")
+        if (dataHTTPS != null) assertEquals(dataHTTPS.status, "OK")
+    }
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can get wallet sync data with start height, start timestamp and block count`() = runTest {
+        // Arrange
+        val requestData = WalletSyncDataRequest(startHeight = 10000, startTimestamp = 100000, blockCount = 50)
+
+        // Act
+        val dataHTTP = clientHTTP.getWalletSyncData(requestData)
+        val dataHTTPS = clientHTTPS.getWalletSyncData(requestData)
+
+        // Assert
+        if (dataHTTP != null) assertEquals(dataHTTP.status, "OK")
+        if (dataHTTPS != null) assertEquals(dataHTTPS.status, "OK")
     }
 }
