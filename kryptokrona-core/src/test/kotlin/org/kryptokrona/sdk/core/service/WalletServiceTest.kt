81c51c4851935f9aca828242134a5d771ee913d1
mjovanc <mjovanc@icloud.com>
2023-04-25 21:02:34 +0200

    Adding WalletServiceTest

--- kryptokrona-core/src/test/kotlin/org/kryptokrona/sdk/core/service/WalletServiceTest.kt
+++ kryptokrona-core/src/test/kotlin/org/kryptokrona/sdk/core/service/WalletServiceTest.kt
@@ -1,4 +1,23 @@
 package org.kryptokrona.sdk.core.service
 
+import org.junit.jupiter.api.Test
+
+/**
+ * Tests for the WalletService class.
+ *
+ * @author Marcus Cvjeticanin
+ * @since 0.2.0
+ */
 class WalletServiceTest {
+
+    @Test
+    fun `can save wallet to file`() {
+        TODO()
+    }
+
+    @Test
+    fun `can load wallet from file`() {
+        TODO()
+    }
+
 }
