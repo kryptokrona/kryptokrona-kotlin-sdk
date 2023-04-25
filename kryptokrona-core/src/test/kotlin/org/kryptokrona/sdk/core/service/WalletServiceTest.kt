a3048ca8dfd4e48e4e4fe952d817f1b0e0e71132
mjovanc <mjovanc@icloud.com>
2023-04-25 20:58:03 +0200

    Adding WalletFile data class to kryptokrona-util lib

---
+++ kryptokrona-core/src/test/kotlin/org/kryptokrona/sdk/core/service/WalletServiceTest.kt
@@ -0,0 +1,4 @@
+package org.kryptokrona.sdk.core.service
+
+class WalletServiceTest {
+}
---
+++ kryptokrona-util/src/main/kotlin/org/kryptokrona/sdk/util/model/wallet/WalletFile.kt
@@ -0,0 +1,12 @@
+package org.kryptokrona.sdk.util.model.wallet
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Wallet file model.
+ *
+ * @author Marcus Cvjeticanin
+ * @since 0.2.0
+ */
+@Serializable
+data class WalletFile()
