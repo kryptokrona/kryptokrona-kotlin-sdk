bc474c0e4af6f95de31cf30a25fc72fd5f43d64c
mjovanc <mjovanc@icloud.com>
2023-04-26 11:37:41 +0200

    Refactoring model Wallet

--- kryptokrona-crypto/src/main/kotlin/org/kryptokrona/sdk/crypto/model/Wallet.kt
+++ kryptokrona-crypto/src/main/kotlin/org/kryptokrona/sdk/crypto/model/Wallet.kt
@@ -28,7 +28,7 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-package org.kryptokrona.sdk.crypto.model.wallet
+package org.kryptokrona.sdk.crypto.model
 
 import org.kryptokrona.sdk.crypto.model.TransactionInput
 import org.kryptokrona.sdk.crypto.model.UnconfirmedInput
