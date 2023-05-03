407cb214fe09f9b968ddd8170af5beb799688383
mjovanc <mjovanc@icloud.com>
2023-05-03 21:28:17 +0200

    Updating layouts and changing default color

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,72 +1,77 @@
 <script>
-  import '@svelteness/kit-docs/client/polyfills/index.js';
-  import '@svelteness/kit-docs/client/styles/normalize.css';
-  import '@svelteness/kit-docs/client/styles/fonts.css';
-  import '@svelteness/kit-docs/client/styles/theme.css';
-  import '@svelteness/kit-docs/client/styles/vars.css';
+    import "@svelteness/kit-docs/client/polyfills/index.js";
+    import "@svelteness/kit-docs/client/styles/normalize.css";
+    import "@svelteness/kit-docs/client/styles/fonts.css";
+    import "@svelteness/kit-docs/client/styles/theme.css";
+    import "@svelteness/kit-docs/client/styles/vars.css";
 
-  import { page } from '$app/stores';
-  import SvelteLogo from '$img/svelte-horizontal.svg?raw';
+    import { page } from "$app/stores";
+    import SvelteLogo from "$img/svelte-horizontal.svg?raw";
 
-  import { Button, KitDocs, KitDocsLayout, createSidebarContext } from '@svelteness/kit-docs';
+    import {
+        Button,
+        KitDocs,
+        KitDocsLayout,
+        createSidebarContext,
+    } from "@svelteness/kit-docs";
 
-  /** @type {import('./$types').LayoutData} */
-  export let data;
+    /** @type {import('./$types').LayoutData} */
+    export let data;
 
-  $: ({ meta, sidebar } = data);
+    $: ({ meta, sidebar } = data);
 
-  /** @type {import('@svelteness/kit-docs').NavbarConfig} */
-  const navbar = {
-    links: [{ title: 'Documentation', slug: '/docs', match: /\/docs/ }],
-  };
+    /** @type {import('@svelteness/kit-docs').NavbarConfig} */
+    const navbar = {
+        links: [{ title: "Documentation", slug: "/docs", match: /\/docs/ }],
+    };
 
-  const { activeCategory } = createSidebarContext(sidebar);
+    const { activeCategory } = createSidebarContext(sidebar);
 
-  $: category = $activeCategory ? `${$activeCategory}: ` : '';
-  $: title = meta ? `${category}${meta.title} | KitDocs` : null;
-  $: description = meta?.description;
+    $: category = $activeCategory ? `${$activeCategory}: ` : "";
+    $: title = meta ? `${category}${meta.title} | KitDocs` : null;
+    $: description = meta?.description;
 </script>
 
 <svelte:head>
-  {#key $page.url.pathname}
-    {#if title}
-      <title>{title}</title>
-    {/if}
-    {#if description}
-      <meta name="description" content={description} />
-    {/if}
-  {/key}
+    {#key $page.url.pathname}
+        {#if title}
+            <title>{title}</title>
+        {/if}
+        {#if description}
+            <meta name="description" content={description} />
+        {/if}
+    {/key}
 </svelte:head>
 
 <KitDocs {meta}>
-  <KitDocsLayout {navbar} {sidebar}>
-    <div class="logo" slot="navbar-left">
-      <Button href="/">
-        {@html SvelteLogo}
-      </Button>
-    </div>
+    <KitDocsLayout {navbar} {sidebar}>
+        <div class="logo" slot="navbar-left">
+            <Button href="/">
+                {@html SvelteLogo}
+            </Button>
+        </div>
 
-    <slot />
-  </KitDocsLayout>
+        <slot />
+    </KitDocsLayout>
 </KitDocs>
 
 <style>
-  :global(:root) {
-    --kd-color-brand-rgb: 233, 127, 6;
-  }
+    :global(:root) {
+        --kd-color-brand-rgb: 127 82 255;
+    }
 
-  :global(:root.dark) {
-    --kd-color-brand-rgb: 213, 149, 76;
-  }
+    :global(:root.dark) {
+        --kd-color-brand-rgb: 127 82 255;
+    }
 
-  .logo :global(a) {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+    .logo :global(a) {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
-  .logo :global(svg) {
-    height: 36px;
-    overflow: hidden;
-  }
+    .logo :global(svg) {
+        height: 36px;
+        overflow: hidden;
+    }
 </style>
