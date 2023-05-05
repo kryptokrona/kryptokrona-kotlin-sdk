708411c171309039c9ef40d7a279c5f0ed1f36d8
appelskrutt34 <ebba1430@hotmail.com>
2023-05-05 19:50:35 +0200

    changed to adapter-auto for vercel

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,17 +1,17 @@
-import adapter from '@sveltejs/adapter-static'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /**
  * @type {import('@sveltejs/kit').Config}
  */
 const config = {
-  extensions: ['.svelte', '.md'],
+  extensions: [".svelte", ".md"],
   preprocess: [vitePreprocess()],
   kit: {
     adapter: adapter({
-      pages: 'dist',
+      pages: "dist",
     }),
   },
-}
+};
 
-export default config
+export default config;
