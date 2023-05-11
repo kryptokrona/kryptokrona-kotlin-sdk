133d34e715501de73c69276225fc3f58d7d1074c
appelskrutt34 <ebba1430@hotmail.com>
2023-05-11 16:15:22 +0200

    added footer

---
+++ website/src/components/Footer.svelte
@@ -0,0 +1,7 @@
+<div
+  style="background-color: var(--box-bg); text-align: center; position:absolute; bottom: 0; right: 0; left: 0;"
+>
+  <p style="padding: .2em; color: var(--subtitle-color) ">
+    Copyright © 2022-present, The Kryptokrona Developers
+  </p>
+</div>
