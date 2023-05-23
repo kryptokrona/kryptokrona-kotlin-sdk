b65bd319aa5997b3b3460c8ccf78538fda6b8190
mjovanc <mjovanc@icloud.com>
2023-05-23 13:34:02 +0200

    Adding RandomOutput and TxInputAndOwner data class

--- kryptokrona-util/src/main/kotlin/org/kryptokrona/sdk/util/model/transaction/TransactionInput.kt
+++ kryptokrona-util/src/main/kotlin/org/kryptokrona/sdk/util/model/transaction/TransactionInput.kt
@@ -28,7 +28,7 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-package org.kryptokrona.sdk.wallet.wallet
+package org.kryptokrona.sdk.util.model.transaction
 
 /**
  * Transaction input is an input that is confirmed in the blockchain.
