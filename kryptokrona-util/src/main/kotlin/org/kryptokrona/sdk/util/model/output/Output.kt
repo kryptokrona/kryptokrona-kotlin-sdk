82503fc62d93de6fb4a8e818f590547726ba58fa
mjovanc <mjovanc@icloud.com>
2023-05-23 13:48:03 +0200

    Adding Output data class

--- kryptokrona-util/src/main/kotlin/org/kryptokrona/sdk/util/model/output/Output.kt
+++ kryptokrona-util/src/main/kotlin/org/kryptokrona/sdk/util/model/output/Output.kt
@@ -1,3 +1,75 @@
+// Copyright (c) 2022-2023, The Kryptokrona Developers
+//
+// Written by Marcus Cvjeticanin
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list
+//    of conditions and the following disclaimer in the documentation and/or other
+//    materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors may be
+//    used to endorse or promote products derived from this software without specific
+//    prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package org.kryptokrona.sdk.util.model.output
 
-data class Output()
+import org.kryptokrona.sdk.util.model.input.GeneratedInput
+
+/**
+ * Output.
+ *
+ * @author Marcus Cvjeticanin
+ * @since 0.3.0
+ */
+data class Output(
+    /**
+     * The output key
+     */
+    val key: String,
+    /**
+     * The output index (position) in the transaction
+     */
+    val index: Long,
+    /**
+     * The output global index
+     */
+    val globalIndex: Long,
+    /**
+     * The output amount
+     */
+    val amount: Double,
+    /**
+     * The output type
+     */
+    val type: Long? = null,
+    /**
+     * The output key image
+     */
+    val keyImage: String? = null,
+    /**
+     * The GeneratedInput of the output (if it belongs to us)
+     */
+    val input: GeneratedInput? = null,
+    /**
+     * Defines whether this is a partial key image
+     */
+    val isPartialKeyImage: Boolean? = null
+)
+
