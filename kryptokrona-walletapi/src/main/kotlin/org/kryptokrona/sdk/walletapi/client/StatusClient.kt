0c47d96787ce79538f5df23043891df9b4ff1102
mjovanc <mjovanc@icloud.com>
2023-05-29 22:52:20 +0200

    Adding Get request for StatusClient

--- kryptokrona-walletapi/src/main/kotlin/org/kryptokrona/sdk/walletapi/client/StatusClient.kt
+++ kryptokrona-walletapi/src/main/kotlin/org/kryptokrona/sdk/walletapi/client/StatusClient.kt
@@ -30,8 +30,19 @@
 
 package org.kryptokrona.sdk.walletapi.client
 
+import io.ktor.client.call.*
+import io.ktor.client.plugins.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.http.headers
+import io.ktor.serialization.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.kryptokrona.sdk.walletapi.common.HttpClient
 import org.kryptokrona.sdk.walletapi.model.WalletApi
+import org.kryptokrona.sdk.walletapi.model.response.StatusResponse
 import org.slf4j.LoggerFactory
+import java.nio.channels.UnresolvedAddressException
 
 /**
  * Status client
@@ -43,4 +54,36 @@ import org.slf4j.LoggerFactory
 class StatusClient(private val walletApi: WalletApi) {
 
     private val logger = LoggerFactory.getLogger("StatusClient")
+
+    /**
+     * Status a wallet.
+     *
+     * @author Marcus Cvjeticanin
+     * @since 0.3.0
+     * @return StatusResponse
+     */
+    suspend fun openWallet(): StatusResponse? {
+        val builder = HttpRequestBuilder().apply {
+            method = HttpMethod.Get
+            walletApi.ssl.let {
+                if (it) {
+                    url.takeFrom("https://${walletApi.hostName}:${walletApi.port}/status")
+                } else {
+                    url.takeFrom("http://${walletApi.hostName}:${walletApi.port}/status")
+                }
+            }
+        }
+
+        try {
+            return HttpClient.client.get(builder).body<StatusResponse>()
+        } catch (e: HttpRequestTimeoutException) {
+            logger.error("Error getting wallet status from Wallet API. Could not reach the server.", e)
+        } catch (e: UnresolvedAddressException) {
+            logger.error("Error getting wallet status from Wallet API. Could not resolve the address.", e)
+        } catch (e: JsonConvertException) {
+            logger.error("Error getting wallet status from Wallet API. Could not parse the response.", e)
+        }
+
+        return null
+    }
 }
