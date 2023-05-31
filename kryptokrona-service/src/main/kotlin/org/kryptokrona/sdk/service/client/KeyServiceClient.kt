723e8bb3c887b59764e877257286260d7d581bae
mjovanc <mjovanc@icloud.com>
2023-06-01 00:43:38 +0200

    Implementing KeyServiceClient

--- kryptokrona-service/src/main/kotlin/org/kryptokrona/sdk/service/client/KeyServiceClient.kt
+++ kryptokrona-service/src/main/kotlin/org/kryptokrona/sdk/service/client/KeyServiceClient.kt
@@ -30,7 +30,26 @@
 
 package org.kryptokrona.sdk.service.client
 
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.kryptokrona.sdk.service.common.HttpClient
 import org.kryptokrona.sdk.service.model.Service
+import org.kryptokrona.sdk.service.model.request.StatusRequest
+import org.kryptokrona.sdk.service.model.request.key.GetSpendKeysRequest
+import org.kryptokrona.sdk.service.model.request.key.GetViewKeyRequest
+import org.kryptokrona.sdk.service.model.request.mnemonic.GetMnemonicSeedRequest
+import org.kryptokrona.sdk.service.model.response.StatusResponse
+import org.kryptokrona.sdk.service.model.response.key.GetSpendKeysResponse
+import org.kryptokrona.sdk.service.model.response.key.GetViewKeyResponse
+import org.kryptokrona.sdk.service.model.response.mnemonic.GetMnemonicSeedResponse
+import org.slf4j.LoggerFactory
+import java.nio.channels.UnresolvedAddressException
 
 /**
  * Key Service Client
@@ -41,9 +60,117 @@ import org.kryptokrona.sdk.service.model.Service
  */
 class KeyServiceClient(private val service: Service) {
 
-    // getSpendKeys
+    private val logger = LoggerFactory.getLogger("KeyServiceClient")
 
-    // getViewKey
+    suspend fun getSpendKeys(getSpendKeysRequest: GetSpendKeysRequest): GetSpendKeysResponse? {
+        val jsonBody = Json.encodeToString(getSpendKeysRequest)
 
-    // getMnemonicSeed
+        val builder = HttpRequestBuilder().apply {
+            method = HttpMethod.Post
+            service.ssl.let {
+                if (it) {
+                    url.takeFrom("https://${service.hostName}:${service.port}/json_rpc")
+                } else {
+                    url.takeFrom("http://${service.hostName}:${service.port}/json_rpc")
+                }
+            }
+            contentType(ContentType.Application.Json)
+            headers {
+                append("Content-Length", jsonBody.length.toString())
+            }
+            setBody(jsonBody)
+        }
+
+        try {
+            return HttpClient.client.post(builder).body<GetSpendKeysResponse>()
+        } catch (e: HttpRequestTimeoutException) {
+            logger.error("Error getting spend keys. Could not reach the server.", e)
+        } catch (e: UnresolvedAddressException) {
+            logger.error("Error getting spend keys. Could not resolve the address.", e)
+        } catch (e: JsonConvertException) {
+            logger.error("Error getting spend keys. Could not parse the response.", e)
+        }
+
+        return null
+    }
+
+    /**
+     * Get the view key from the wallet.
+     *
+     * @author Marcus Cvjeticanin
+     * @since 0.3.0
+     * @param getViewKeyRequest The request to send to the wallet.
+     * @return The response from the wallet.
+     */
+    suspend fun getViewKey(getViewKeyRequest: GetViewKeyRequest): GetViewKeyResponse? {
+        val jsonBody = Json.encodeToString(getViewKeyRequest)
+
+        val builder = HttpRequestBuilder().apply {
+            method = HttpMethod.Post
+            service.ssl.let {
+                if (it) {
+                    url.takeFrom("https://${service.hostName}:${service.port}/json_rpc")
+                } else {
+                    url.takeFrom("http://${service.hostName}:${service.port}/json_rpc")
+                }
+            }
+            contentType(ContentType.Application.Json)
+            headers {
+                append("Content-Length", jsonBody.length.toString())
+            }
+            setBody(jsonBody)
+        }
+
+        try {
+            return HttpClient.client.post(builder).body<GetViewKeyResponse>()
+        } catch (e: HttpRequestTimeoutException) {
+            logger.error("Error getting view key. Could not reach the server.", e)
+        } catch (e: UnresolvedAddressException) {
+            logger.error("Error getting view key. Could not resolve the address.", e)
+        } catch (e: JsonConvertException) {
+            logger.error("Error getting view key. Could not parse the response.", e)
+        }
+
+        return null
+    }
+
+    /**
+     * Get the mnemonic seed of the wallet.
+     *
+     * @author Marcus Cvjeticanin
+     * @since 0.3.0
+     * @param getMnemonicSeedRequest The request.
+     * @return The response.
+     */
+    suspend fun getMnemonicSeed(getMnemonicSeedRequest: GetMnemonicSeedRequest): GetMnemonicSeedResponse? {
+        val jsonBody = Json.encodeToString(getMnemonicSeedRequest)
+
+        val builder = HttpRequestBuilder().apply {
+            method = HttpMethod.Post
+            service.ssl.let {
+                if (it) {
+                    url.takeFrom("https://${service.hostName}:${service.port}/json_rpc")
+                } else {
+                    url.takeFrom("http://${service.hostName}:${service.port}/json_rpc")
+                }
+            }
+            contentType(ContentType.Application.Json)
+            headers {
+                append("Content-Length", jsonBody.length.toString())
+            }
+            setBody(jsonBody)
+        }
+
+        try {
+            return HttpClient.client.post(builder).body<GetMnemonicSeedResponse>()
+        } catch (e: HttpRequestTimeoutException) {
+            logger.error("Error getting mnemonic seed. Could not reach the server.", e)
+        } catch (e: UnresolvedAddressException) {
+            logger.error("Error getting mnemonic seed. Could not resolve the address.", e)
+        } catch (e: JsonConvertException) {
+            logger.error("Error getting mnemonic seed. Could not parse the response.", e)
+        }
+
+        return null
+    }
 }
