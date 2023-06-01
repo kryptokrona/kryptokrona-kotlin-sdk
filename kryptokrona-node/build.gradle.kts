e24c82850467c1f86bf59792bc25b543f134f93b
mjovanc <mjovanc@icloud.com>
2023-06-01 21:56:22 +0200

    Upgrading Kover 0.7.0-Alpha -> 0.7.0

--- kryptokrona-node/build.gradle.kts
+++ kryptokrona-node/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.serialization") version "1.8.21"
     id("org.jetbrains.dokka")
-    id("org.jetbrains.kotlinx.kover") version "0.7.0-Alpha"
+    id("org.jetbrains.kotlinx.kover") version "0.7.0"
     `java-library`
     `maven-publish`
     signing
@@ -59,23 +59,25 @@ koverReport {
         }
     }
 
-    verify {
-        onCheck = true
-        rule {
-            isEnabled = true
-            entity = kotlinx.kover.gradle.plugin.dsl.GroupingEntityType.APPLICATION
+    defaults {
+        verify {
+            onCheck = true
+            rule {
+                isEnabled = true
+                entity = kotlinx.kover.gradle.plugin.dsl.GroupingEntityType.APPLICATION
 
-            filters {
-                excludes {
-                    classes("org.kryptokrona.sdk.http.model.*")
+                filters {
+                    excludes {
+                        classes("org.kryptokrona.sdk.http.model.*")
+                    }
                 }
-            }
 
-            bound {
-                minValue = 60
-                maxValue = 90
-                metric = kotlinx.kover.gradle.plugin.dsl.MetricType.LINE
-                aggregation = kotlinx.kover.gradle.plugin.dsl.AggregationType.COVERED_PERCENTAGE
+                bound {
+                    minValue = 60
+                    maxValue = 90
+                    metric = kotlinx.kover.gradle.plugin.dsl.MetricType.LINE
+                    aggregation = kotlinx.kover.gradle.plugin.dsl.AggregationType.COVERED_PERCENTAGE
+                }
             }
         }
     }
