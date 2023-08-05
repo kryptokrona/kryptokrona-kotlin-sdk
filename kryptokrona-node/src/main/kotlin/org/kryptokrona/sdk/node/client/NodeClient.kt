704da68f48bb2aa2e91d3b90dfa738135627d736
mjovanc <mjovanc@icloud.com>
2023-08-05 17:06:58 +0200

    Updating NodeClient

--- kryptokrona-node/src/main/kotlin/org/kryptokrona/sdk/node/client/NodeClient.kt
+++ kryptokrona-node/src/main/kotlin/org/kryptokrona/sdk/node/client/NodeClient.kt
@@ -77,6 +77,8 @@ class NodeClient(private val node: Node) {
             logger.error("Error getting node information. Could not reach the server.", e)
         } catch (e: UnresolvedAddressException) {
             logger.error("Error getting node information. Could not resolve the address.", e)
+        } catch (e: ConnectTimeoutException) {
+            logger.error("Error getting node information. Connection timeout.", e)
         }
 
         return isSuccess
