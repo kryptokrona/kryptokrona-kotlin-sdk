5eb37f0cca33cc7e5f408f5b8b55be3a3e190917
mjovanc <mjovanc@icloud.com>
2023-08-11 19:33:07 +0200

    Adding skeleton unit tests in AddressServiceClientTest

--- kryptokrona-service/src/test/kotlin/org/kryptokrona/sdk/service/client/AddressServiceClientTest.kt
+++ kryptokrona-service/src/test/kotlin/org/kryptokrona/sdk/service/client/AddressServiceClientTest.kt
@@ -1,4 +1,36 @@
 package org.kryptokrona.sdk.service.client
 
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+import org.kryptokrona.sdk.service.model.Service
+
 class AddressServiceClientTest {
+
+    private val service = Service("localhost", 8070, "false", "", false)
+
+    private val client = AddressServiceClient(service)
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can create a new address`() = runTest {
+
+    }
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can delete an address`() = runTest {
+
+    }
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can get a list of addresses for the wallet`() = runTest {
+
+    }
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can create an integrated address`() = runTest {
+
+    }
 }
