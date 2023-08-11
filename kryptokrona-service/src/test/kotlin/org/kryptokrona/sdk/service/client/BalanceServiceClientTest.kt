e74fcda46a3dae5895caac462771add3314d191f
mjovanc <mjovanc@icloud.com>
2023-08-11 19:33:21 +0200

    Adding skeleton unit tests in BalanceServiceClientTest

--- kryptokrona-service/src/test/kotlin/org/kryptokrona/sdk/service/client/BalanceServiceClientTest.kt
+++ kryptokrona-service/src/test/kotlin/org/kryptokrona/sdk/service/client/BalanceServiceClientTest.kt
@@ -1,4 +1,18 @@
 package org.kryptokrona.sdk.service.client
 
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+import org.kryptokrona.sdk.service.model.Service
+
 class BalanceServiceClientTest {
+
+    private val service = Service("localhost", 8070, "false", "", false)
+
+    private val client = BalanceServiceClient(service)
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can get balance`() = runTest {
+
+    }
 }
