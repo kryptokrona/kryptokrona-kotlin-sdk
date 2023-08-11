3cbb83178156d10c7b445a30114552fe4cc17dab
mjovanc <mjovanc@icloud.com>
2023-08-11 19:34:07 +0200

    Adding skeleton unit tests in KeyServiceClientTest

--- kryptokrona-service/src/test/kotlin/org/kryptokrona/sdk/service/client/KeyServiceClientTest.kt
+++ kryptokrona-service/src/test/kotlin/org/kryptokrona/sdk/service/client/KeyServiceClientTest.kt
@@ -1,4 +1,30 @@
 package org.kryptokrona.sdk.service.client
 
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+import org.kryptokrona.sdk.service.model.Service
+
 class KeyServiceClientTest {
+
+    private val service = Service("localhost", 8070, "false", "", false)
+
+    private val client = KeyServiceClient(service)
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can get spend keys`() = runTest {
+
+    }
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can get the view key from the wallet`() = runTest {
+
+    }
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can get the mnemonic seed of the wallet`() = runTest {
+
+    }
 }
