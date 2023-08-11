287df9d8b36effa0e15bf196cc14c7f6fcfdc727
mjovanc <mjovanc@icloud.com>
2023-08-11 19:34:19 +0200

    Adding skeleton unit tests in StatusServiceClientTest

--- kryptokrona-service/src/test/kotlin/org/kryptokrona/sdk/service/client/StatusServiceClientTest.kt
+++ kryptokrona-service/src/test/kotlin/org/kryptokrona/sdk/service/client/StatusServiceClientTest.kt
@@ -1,4 +1,18 @@
 package org.kryptokrona.sdk.service.client
 
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+import org.kryptokrona.sdk.service.model.Service
+
 class StatusServiceClientTest {
+
+    private val service = Service("localhost", 8070, "false", "", false)
+
+    private val client = StatusServiceClient(service)
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can get status`() = runTest {
+
+    }
 }
