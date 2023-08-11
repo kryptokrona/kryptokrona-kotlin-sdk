616bacd0ca2efb3214d00d061618311d13e95483
mjovanc <mjovanc@icloud.com>
2023-08-11 19:34:48 +0200

    Adding skeleton unit tests in WalletServiceClientTest

--- kryptokrona-service/src/test/kotlin/org/kryptokrona/sdk/service/client/WalletServiceClientTest.kt
+++ kryptokrona-service/src/test/kotlin/org/kryptokrona/sdk/service/client/WalletServiceClientTest.kt
@@ -1,4 +1,24 @@
 package org.kryptokrona.sdk.service.client
 
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+import org.kryptokrona.sdk.service.model.Service
+
 class WalletServiceClientTest {
+
+    private val service = Service("localhost", 8070, "false", "", false)
+
+    private val client = WalletServiceClient(service)
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can save wallet`() = runTest {
+
+    }
+
+    @Test
+    @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+    fun `can reset wallet`() = runTest {
+
+    }
 }
