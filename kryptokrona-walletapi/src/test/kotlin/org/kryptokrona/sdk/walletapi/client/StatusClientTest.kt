23a213232d6b16db560de1b51eea82b33f2548c7
mjovanc <mjovanc@icloud.com>
2023-08-13 10:56:19 +0200

    Implementing StatusClientTest

--- kryptokrona-walletapi/src/test/kotlin/org/kryptokrona/sdk/walletapi/client/StatusClientTest.kt
+++ kryptokrona-walletapi/src/test/kotlin/org/kryptokrona/sdk/walletapi/client/StatusClientTest.kt
@@ -1,8 +1,12 @@
 package org.kryptokrona.sdk.walletapi.client
 
 import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.kryptokrona.sdk.walletapi.model.WalletApi
+import kotlin.test.DefaultAsserter.assertTrue
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class StatusClientTest {
 
@@ -13,6 +17,12 @@ class StatusClientTest {
     @Test
     @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
     fun `can get wallet status`() = runTest {
+        // Arrange
 
+        // Act
+        val status = client.walletStatus()
+
+        // Assert
+        assertNotNull(status?.walletBlockCount)
     }
 }
