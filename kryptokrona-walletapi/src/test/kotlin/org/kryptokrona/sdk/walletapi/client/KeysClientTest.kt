fd8abe3edd90927178dc443e0ffbbcf523e03a36
mjovanc <mjovanc@icloud.com>
2023-08-13 11:13:05 +0200

    Implementing KeysClientTest

--- kryptokrona-walletapi/src/test/kotlin/org/kryptokrona/sdk/walletapi/client/KeysClientTest.kt
+++ kryptokrona-walletapi/src/test/kotlin/org/kryptokrona/sdk/walletapi/client/KeysClientTest.kt
@@ -3,6 +3,8 @@ package org.kryptokrona.sdk.walletapi.client
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 import org.kryptokrona.sdk.walletapi.model.WalletApi
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 class KeysClientTest {
 
@@ -16,27 +18,41 @@ class KeysClientTest {
         // Arrange
 
         // Act
+        val sharedPrivateViewKey = client.sharedPrivateViewKey()
 
         // Assert
+        assertNotNull(sharedPrivateViewKey)
     }
 
     @Test
     @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
     fun `can get spend keys`() = runTest {
         // Arrange
+        val correctAddress = ""
+        val incorrectAddress = "SEKReV7rDhaj9UV8NYgesQLyAVoddy4foZQzqg2Ci4YrdUNB3qFUzjCQeEe85es2yuC8wE9kGzhcKU23A7Qa5qm9h4CFbKh3umH"
 
         // Act
+        val spendKeysWithCorrectAddress = client.getSpendKeys(correctAddress)
+        val spendKeysWithIncorrectAddress = client.getSpendKeys(incorrectAddress)
 
         // Assert
+        assertNotNull(spendKeysWithCorrectAddress)
+        assertNull(spendKeysWithIncorrectAddress)
     }
 
     @Test
     @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
     fun `can get mnemonic seed`() = runTest {
         // Arrange
+        val correctAddress = ""
+        val incorrectAddress = "SEKReV7rDhaj9UV8NYgesQLyAVoddy4foZQzqg2Ci4YrdUNB3qFUzjCQeEe85es2yuC8wE9kGzhcKU23A7Qa5qm9h4CFbKh3umH"
 
         // Act
+        val mnemonicSeedWithCorrectAddress = client.getMnemonicSeed(correctAddress)
+        val mnemonicSeedWithIncorrectAddress = client.getMnemonicSeed(incorrectAddress)
 
         // Assert
+        assertNotNull(mnemonicSeedWithCorrectAddress)
+        assertNull(mnemonicSeedWithIncorrectAddress)
     }
 }
